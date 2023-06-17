12afec26c3821998e00d8213db0ca0d7694fb936
ajay.panthagani <ajay.panthagani@gojek.com>
2023-06-17 10:00:06 +0530

    build: initial setup

---
+++ app/src/main/java/redis/clone/App.java
@@ -0,0 +1,11 @@
+package redis.clone;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
